fc0771d79d81dacf65e00d431d0b46f5752ffe21
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-10-11 20:14:26 -0500

    MVP Stretch & Questions

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,19 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+
+# Loop through and store the weights in a dict
+# loop through and find the weight limits
+# store the weight as a key
+# Check for weight limits
+# Use Min() and Max() built - in functions to compare?
+
+    data = {}
+    for i in range(len(weights)):
+        data[weights[i]] = i
+    for i in range(len(weights)):
+        dif = limit-weights[i]
+        if dif in data:
+            return (max(i, data[dif]), min(i, data[dif]))
 
     return None
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,22 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+
+    #loop through queries
+    for item in queries:
+        #add queries to cache
+        cache[item] = [item]
+
+    #create list for results
+    result = []
+
+    #loop through files
+    for item in files:
+        #check if last part of file path is in cache
+        if item.split("/")[-1] in cache:
+            #if it is, append it to the list
+            result.append(item)
 
     return result
 
